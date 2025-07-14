fcdacbb47de6f76e63aecc844fbf4d0067ef8202
ckani <chileshekani@yahoo.com>
2025-07-14 19:12:01 +0100

    Add Inter font to project and Tailwind config
    Imported the Inter font from Google Fonts in index.css and set it as the default sans font in Tailwind configuration for consistent typography.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+  sans: ['Inter', 'sans-serif'],
+},
+
   },
   plugins: [],
 };
