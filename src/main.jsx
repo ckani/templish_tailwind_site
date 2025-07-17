d87ee81703f7ffa3c0c7baf39a67961111a4fe19
ckani <chileshekani@yahoo.com>
2025-07-17 19:54:41 +0100

    update with hero

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 import Home from './pages/Home';
 import Reset from './pages/Reset';
-import './index.css';
+import About from './pages/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/reset" element={<Reset />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>,
 );
